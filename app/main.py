e037b90412a35a4ca445a5398cc0ce00efa3f9c0
ReezanVisram <59345266+ReezanVisram@users.noreply.github.com>
2020-08-02 12:05:28 -0400

    Added base template for / route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from app.Models import db
-from flask_bootstrap import Bootstrap
 
 from app.Blueprints.User.routes import userBlueprint
 from app.Blueprints.Task.routes import taskBlueprint
@@ -8,7 +7,6 @@ from app.Blueprints.Habit.routes import habitBlueprint
 
 app = Flask(__name__)
 db.init_app(app)
-Bootstrap(app)
 
 
 if (app.config['ENV'] == 'production'):
@@ -22,7 +20,7 @@ else:
 
 @app.route('/')
 def homeRoute():
-    return render_template('index.html')
+    return render_template('home.html')
 
 app.register_blueprint(userBlueprint, url_prefix='/user')
 app.register_blueprint(taskBlueprint, url_prefix='/task')
